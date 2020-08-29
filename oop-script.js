1653f375e81239e917f5adbadcffe3f5db0d31fd
jennethydyrova <jennethydyrovaa@gmail.com>
2020-08-29 22:45:58 +0300

    fixed fetchActors part

--- oop-script.js
+++ oop-script.js
@@ -3,7 +3,7 @@
 class App {
   static async run() {
     const movies = await APIService.fetchMovies();
-     console.log(movies);
+    console.log(movies);
     HomePage.renderMovies(movies);
   }
 }
@@ -26,23 +26,23 @@ class APIService {
 
   static async fetchActors(movieId) {
     //   console.log(movieId)
-    const url = APIService._constructUrl(`movie/${movieId}/credits`); 
+    const url = APIService._constructUrl(`movie/${movieId}/credits`);
     const response = await fetch(url);
     // console.log(response)
     const data = await response.json();
     //  console.log(data)
-     const arrWithActorInfo = [];
-     for (let i = 0; i < 5; i++) {
-      arrWithActorInfo.push(new Actors(data.cast[i]))
+    const arrWithActorInfo = [];
+    for (let i = 0; i < 5; i++) {
+      arrWithActorInfo.push(new Actors(data.cast[i]));
       // arrWithActorInfo.push(data.cast[i].character)
       // arrWithActorInfo.push(data.cast[i].name)
-     }
+    }
     //  return data.cast.map((actor) => {
     //    console.log(actor);
     //   new Actors(actor)
     // });
     // console.log(arrWithActorInfo);
-    return arrWithActorInfo
+    return arrWithActorInfo;
     //arrWithActorInfo.forEach(ele => new Actors(ele))
   }
   static _constructUrl(path) {
@@ -75,23 +75,22 @@ class HomePage {
 class Movies {
   static async run(movie) {
     const movieData = await APIService.fetchMovie(movie.id);
-    MoviePage.renderMovieSection(movieData);
-    const actorData =  await APIService.fetchActors(movie.id);
-    console.log(actorData)
-    MoviePage.renderMovieSection(actorData);
+    const actorData = await APIService.fetchActors(movie.id);
+    MoviePage.renderMovieSection(movieData, actorData);
   }
 }
 
 class MoviePage {
   static container = document.getElementById("container");
-  static renderMovieSection(movie) {
-    MovieSection.renderMovie(movie);
+  static renderMovieSection(movie, actor) {
+    MovieSection.renderMovie(movie, actor);
   }
 }
 
 class MovieSection {
-  static renderMovie(movie) {
-    console.log(movie)
+  static renderMovie(movie, actor) {
+    console.log(movie);
+    console.log(actor);
     MoviePage.container.innerHTML = `
       <div class="row">
         <div class="col-md-4">
@@ -107,8 +106,17 @@ class MovieSection {
         </div>
       </div>
       <h3>Actors:</h3>
-      <p>Hello</p>
+      <div class="actors row"></div>
     `;
+    for (let i = 0; i < actor.length; i++) {
+      document.querySelector(".actors").innerHTML += `
+        <div class="col-md-2">
+          <img src=${actor[i].backdropPath} width="100px">
+          <h3>${actor[i].name}</h3>
+          <h6>${actor[i].character}</h6>
+    
+      </div>`;
+    }
   }
 }
 
@@ -117,6 +125,7 @@ class Movie {
   constructor(json) {
     this.id = json.id;
     this.title = json.title;
+    this.genres = json.genres;
     this.releaseDate = json.release_date;
     this.runtime = json.runtime + " minutes";
     this.overview = json.overview;
@@ -128,34 +137,36 @@ class Movie {
   }
 }
 
-class Actor {
-  static BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
-  constructor(json) {
-    this.name = json.name;
-    this.gender = json.gender;
-    this.birthday = json.birthday;
-    this.picOfActor = json.profile_path;
-    this.actorPopularity = json.popularity;
-    this.birthday = json.birthday;
-    this.deathday = json.deathday;
-    this.actorBiography = json.biography;
-  }
-  get profilePath() {
-    return this.picOfActor ? Actor.BACKDROP_BASE_URL + this.picOfActor : "";
-  }
-}
+// class Actor {
+//   static BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+//   constructor(json) {
+//     this.name = json.name;
+//     this.gender = json.gender;
+//     this.birthday = json.birthday;
+//     this.picOfActor = json.profile_path;
+//     this.actorPopularity = json.popularity;
+//     this.birthday = json.birthday;
+//     this.deathday = json.deathday;
+//     this.actorBiography = json.biography;
+//   }
+//   get profilePath() {
+//     return this.picOfActor ? Actor.BACKDROP_BASE_URL + this.picOfActor : "";
+//   }
+// }
 
 class Actors {
   static BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
-    constructor(json) {
-      this.name = json.name;
-      this.backdropPath = json.profile_path;
-      this.character = json.character;
-    }
+  constructor(json) {
+    this.name = json.name;
+    this._backdropPath = json.profile_path;
+    this.character = json.character;
+  }
 
-    get backdropPath () {
-      return this.backdropPath ? Actors.BACKDROP_BASE_URL + this.backdropPath : "";
-    }
+  get backdropPath() {
+    return this._backdropPath
+      ? Actors.BACKDROP_BASE_URL + this._backdropPath
+      : "";
+  }
 }
 
 document.addEventListener("DOMContentLoaded", App.run);
@@ -167,7 +178,6 @@ document.addEventListener("DOMContentLoaded", App.run);
 //fetch list of movies actor participated in from https://api.themoviedb.org/3/person/${movie.id}/movie_credits?api_key=542003918769df50083a13c415bbc602&language=en-US
 //create a new class for it, because we will use it several times
 
-
 //for Sunday, Aug, 30
 //
 // create a class called Actors
@@ -177,7 +187,6 @@ document.addEventListener("DOMContentLoaded", App.run);
 // now we have an array of names
 // then in the dom we can some how use some kind of loop to display theese names inside the inner html
 //same steps for actors pictures...
-
 
 //Thoughts and ideas on how to solve stuff
 
